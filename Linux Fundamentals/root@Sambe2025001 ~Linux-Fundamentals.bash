92f6f2c6b20f275c003d0728a50d63d76559eb59
Shriya Hariparsad <shriya.hariparsad@sambeconsulting.com>
2026-03-25 09:09:25 +0200

    This is my first commit

---
+++ Linux Fundamentals/root@Sambe2025001 ~Linux-Fundamentals.bash
@@ -0,0 +1,12 @@
+root@Sambe2025001:~# mkdir Linux-Fundamentals
+root@Sambe2025001:~# cd Linux-Fundamentals/
+root@Sambe2025001:~/Linux-Fundamentals# touch file.txt
+root@Sambe2025001:~/Linux-Fundamentals# ls -la
+total 8
+drwxr-xr-x 2 root root 4096 Mar 25 09:01 .
+drwx------ 6 root root 4096 Mar 25 09:01 ..
+-rw-r--r-- 1 root root    0 Mar 25 09:01 file.txt
+root@Sambe2025001:~/Linux-Fundamentals# echo "This is my Linux Fundamentals course practice" > file.txt
+root@Sambe2025001:~/Linux-Fundamentals# cat file.txt
+This is my Linux Fundamentals course practice
+root@Sambe2025001:~/Linux-Fundamentals#

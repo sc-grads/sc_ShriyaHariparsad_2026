474276d445324260c4f7d977afe8b37463ad6c71
Shriya Hariparsad <shriya.hariparsad@sambeconsulting.com>
2026-03-25 12:02:26 +0200

    Find options

---
+++ Linux Fundamentals/root@Sambe2025001 ~Find options.bash
@@ -0,0 +1,11 @@
+root@Sambe2025001:~/Linux-Fundamentals# find
+.
+./file.txt
+root@Sambe2025001:~/Linux-Fundamentals# find .
+.
+./file.txt
+root@Sambe2025001:~/Linux-Fundamentals# find/usr/sbin -name networkmanager
+-bash: find/usr/sbin: No such file or directory
+root@Sambe2025001:~/Linux-Fundamentals# find/usr/sbin -iname networkmanager
+-bash: find/usr/sbin: No such file or directory
+root@Sambe2025001:~/Linux-Fundamentals#

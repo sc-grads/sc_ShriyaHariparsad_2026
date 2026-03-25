28e8166835d7c507b0ce2ebe0099081b1903ec50
Shriya Hariparsad <shriya.hariparsad@sambeconsulting.com>
2026-03-25 17:54:09 +0200

    Customising shell prompt added

---
+++ Linux Fundamentals/Customising shell prompts added.bash
@@ -0,0 +1,17 @@
+[persist root@Sambe2025001 /]$echo $PS1
+[persist \u@\h \w]$
+[persist root@Sambe2025001 /]$PS1="\u@\h \$ "
+root@Sambe2025001 $ PS1="<\t \u@\h \w>\$"
+<17:37:15 root@Sambe2025001 />$ cd /var/log
+<17:37:51 root@Sambe2025001 /var/log>$ PS1="\d \t \h \W>\$ "
+Wed Mar 25 17:38:30 Sambe2025001 log>$ cd
+Wed Mar 25 17:38:57 Sambe2025001 ~>$ vi .bash_profile
+Wed Mar 25 17:39:37 Sambe2025001 ~>$ $echo $PS1
+\d: command not found
+Wed Mar 25 17:45:51 Sambe2025001 ~>$ $PS1="\u@\h \$ "
+\d: command not found
+Wed Mar 25 17:46:33 Sambe2025001 ~>$ PS1="[persist \u@\h \W]\$"
+[persist root@Sambe2025001 ~]$PS1="[persist \u@\h \W]\$ "
+[persist root@Sambe2025001 ~]$ PS1="\t\n[\h \w]\$ "
+17:51:26
+[Sambe2025001 ~]$

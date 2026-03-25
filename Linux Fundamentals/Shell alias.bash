894cfc5b98cf2f33754c882f5f68b3b5222cc37f
Shriya Hariparsad <shriya.hariparsad@sambeconsulting.com>
2026-03-25 18:18:38 +0200

    shell alias

---
+++ Linux Fundamentals/Shell alias.bash
@@ -0,0 +1,23 @@
+[root@Sambe2025001 ~]$ ls -l
+total 20
+drwxr-xr-x 2 root root 4096 Mar 25 09:01 Linux-Fundamentals
+drwxr-xr-x 2 root root 4096 Mar 25 07:48 dir1
+drwxr-xr-x 2 root root 4096 Mar 25 13:30 my_file
+drwxr-xr-x 2 root root 4096 Mar 25 12:16 my_test
+drwxr-xr-x 3 root root 4096 Mar 25 10:37 testumask
+[root@Sambe2025001 ~]$ alias ll='ls -l'
+[root@Sambe2025001 ~]$ll
+total 20
+drwxr-xr-x 2 root root 4096 Mar 25 09:01 Linux-Fundamentals
+drwxr-xr-x 2 root root 4096 Mar 25 07:48 dir1
+drwxr-xr-x 2 root root 4096 Mar 25 13:30 my_file
+drwxr-xr-x 2 root root 4096 Mar 25 12:16 my_test
+drwxr-xr-x 3 root root 4096 Mar 25 10:37 testumask
+[root@Sambe2025001 ~]$ ls -l
+total 20
+drwxr-xr-x 2 root root 4096 Mar 25 09:01 Linux-Fundamentals
+drwxr-xr-x 2 root root 4096 Mar 25 07:48 dir1
+drwxr-xr-x 2 root root 4096 Mar 25 13:30 my_file
+drwxr-xr-x 2 root root 4096 Mar 25 12:16 my_test
+drwxr-xr-x 3 root root 4096 Mar 25 10:37 testumask
+[root@Sambe2025001 ~]$

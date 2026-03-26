f185edac9bf24506f3c1900c36e3d9e50607294e
Shriya Hariparsad <shriya.hariparsad@sambeconsulting.com>
2026-03-26 13:58:03 +0200

    Shell history and completion tab

---
+++ Linux Fundamentals/Shell history and completion tab.bash
@@ -0,0 +1,655 @@
+[root@Sambe2025001 ~]$ history
+    1  pwd
+    2  PWD
+    3  cd
+    4  man cat
+    5  ls
+    6  exit
+    7  which cat
+    8  which tac
+    9  man -k calendar
+   10  path
+   11  ls
+   12  pwd
+   13  cd tpsreports
+   14  ls
+   15  echo #PATH
+   16  echo $PATH
+   17  which cat
+   18  cat sale.data
+   19  which cat
+   20  mk dir1
+   21  mkdir dir1
+   22  ls
+   23  explorer .exe .
+   24  explorer.exe .
+   25  Linux FundamentalsLinux FundamentalsLinux FundamentalsLinux Fundamentals
+   26  mkdir Linux-Fundamentals
+   27  cd Linux-Fundamentals/
+   28  touch file.txt
+   29  ls -la
+   30  echo "This is my Linux Fundamentals course practice" > file.txt
+   31  cat file.txt
+   32  clear
+   33  cat file.txt
+   34  clear
+   35  ls
+   36  ls -a
+   37  ls -l
+   38  ls -l -a
+   39  ls -a -l
+   40  clear
+   41  ls -l
+   42  ls -lr
+   43  ls -lat
+   44  ls -latr
+   45  ls -R
+   46  tree -d
+   47  ls -d
+   48  ls --color
+   49  ls -R
+   50  tree
+   51  clear
+   52  groups
+   53  Gn
+   54  id -Gn
+   55  clear
+   56  cd ..
+   57  mkdir testumask
+   58  cd testumask
+   59  umask -S
+   60  mkdir a-dir
+   61  touch a-file
+   62  ls -l
+   63  rmdir a-dir
+   64  rm a-file
+   65  umask 007
+   66  umask 0007
+   67  umask -S
+   68  mkdir a-dir
+   69  touch a-file
+   70  ls -l
+   71  cd ..
+   72  cd .
+   73  cd "C:\sc_ShriyaHariparsad_2026\Linux Fundamentals"
+   74  cd ..
+   75  cd Linux-Fundamentals
+   76  cd ~/Linux-Fundamentals/
+   77  clear
+   78  find
+   79  find .
+   80  find/usr/sbin -name networkmanager
+   81  find/usr/sbin -iname networkmanager
+   82  clear
+   83  locate uptime
+   84  find uptime
+   85  clear
+   86  cd ..
+   87  cd
+   88  cat
+   89  cd
+   90  cd mkdir my_folder
+   91  clear
+   92  mkdir my_test
+   93  cd my_test
+   94  touch myfile.txt
+   95  cat myfile.txt
+   96  echo "This is my first line" >myfile.txt
+   97  echo "This is the second" > myfile.txt
+   98  cat myfile.txt
+   99  echo "Here is some more interesting text" > myfile.txt
+  100  echo "Who is there" > myfile.txt
+  101  cat myfile.txt
+  102  clear
+  103  cat file.txt
+  104  clear
+  105  cd file.txt
+  106  cd my_test
+  107  clear
+  108  echo "This is a new line" >> myfile.txt
+  109  cat file.txt
+  110  mkdir my_file
+  111  cd my_file
+  112  touch file.txt
+  113  echo "This is the first line.
+  114  This is the second.
+  115  Here is some more interesting text.
+  116  Knock knock.
+  117  Who's there?
+  118  More filler text.
+  119  The quick brown fox jumps over the lazy dog.
+  120  The dog was rather impressed.
+  121  Roses are red,
+  122  Violets are blue,
+  123  All my base are belong to you.
+  124  Finally, the 12th and last line.
+  125  $ head file.txt
+  126  This is the first line.
+  127  This is the second.
+  128  Here is some more interesting text.
+  129  Knock knock.
+  130  Who's there?
+  131  More filler text.
+  132  The quick brown fox jumps over the lazy dog.
+  133  The dog was rather impressed.
+  134  Roses are red,
+  135  Violets are blue,
+  136  $ tail file.txt
+  137  Here is some more interesting text.
+  138  Knock knock.
+  139  Who's there?
+  140  More filler text.
+  141  The quick brown fox jumps over the lazy dog.
+  142  The dog was rather impressed.
+  143  Roses are red,
+  144  Violets are blue,
+  145  All my base are belong to you.
+  146  Finally, the 12th and last line.
+  147  $ more file.txt
+  148  Here is some more interesting text.
+  149  Knock knock.
+  150  Who's there?
+  151  More filler text." >> file.txt
+  152  cat myfile.txt
+  153  cat file.txt
+  154  head file.txt
+  155  head -2 file.txt
+  156  tail file.txt
+  157  tail -1 file.txt
+  158  nano file.txt
+  159  clear
+  160  vi file.txt
+  161  clear
+  162  emacs file.txt
+  163  emac file.txt
+  164  emacs file.txt
+  165  clear
+  166  echo $PSl
+  167  echo #PSl
+  168  PSl="<\t \u@\h \w>\$ "
+  169  echo $PS1
+  170  clear
+  171  echo $PS1
+  172  PS1="\u@\h \$ "
+  173  PS1="<\t \u@\h \w>\$ "
+  174  cd /var/log
+  175  PS1="\d \t \h \W>\$ "
+  176  cd
+  177  vi .bash_profile
+  178  exit
+  179   PS1="[\u@\h \W]\$"
+  180  clear
+  181   ls -l
+  182   alias ll='ls -l'
+  183  ll
+  184   ls -l
+  185  alias
+  186  alias
+  187  clear
+  188  printenv
+  189  printenv | less
+  190  printenv USER
+  191  echo $USER
+  192  date
+  193  export TZ="US/Pacific"
+  194  date
+  195  unset TZ
+  196  date
+  197  man date
+  198  clear
+  199  echo $PS1
+  200  cd ..
+  201   cd..
+  202  clear
+  203  echo $PS1
+  204  PS1="\u@\h \$ "
+  205  PS1="<\t \u@\h \w>\$"
+  206   cd /var/log
+  207   PS1="\d \t \h \W>\$ "
+  208  cd
+  209  vi .bash_profile
+  210  $echo $PS1
+  211  $PS1="\u@\h \$ "
+  212  PS1="[persist \u@\h \W]\$"
+  213  PS1="[persist \u@\h \W]\$ "
+  214  PS1="\t\n[\h \w]\$ "
+  215  clear
+  216  clear
+  217  sudo -i
+  218  sudo su -
+  219  clear
+  220  exit
+  221  PS1="[\u@\h \W]\$ "
+  222  clear
+  223  ps
+  224  ps -p 517
+  225  ps -f
+  226  ps -e
+  227  ps -ef
+  228  les
+  229  less
+  230  ps -ef |less
+  231  ps -fu root
+  232  ps --forest
+  233  ps -ef --forest
+  234  pstree
+  235  PS1"[\u@\h W]\$ "
+  236  $PS1="[\u@\h W]\$ "
+  237  PS1="[\u@\h W]\$ "
+  238  PS1="[\u@\h \W]\$ "
+  239  pstree
+  240  clear
+  241  pstree
+  242  top
+  243  htop
+  244  clear
+  245  ./long-running-program &
+  246  clear
+  247  jobs
+  248  ps -p
+  249  ./long-running-program &
+  250  ps -p4499
+  251  clear
+  252  ./long-running-program &
+  253  ps -p 4892
+  254  jobs
+  255  clear
+  256  kill -l
+  257  kill -KILL
+  258  kill -KILL ^C
+  259  clear
+  260  kill -l
+  261  kill -KILL ^C
+  262  clear
+  263  kill -l
+  264  ./long-running-program &
+  265  ./long-running-program &
+  266  kill 6489
+  267  kill -9
+  268  clear
+  269  crontab -l
+  270  vi my-cron
+  271  cat my-cron
+  272  crontab my-cron
+  273  crontab -l
+  274  echo $EDITOR
+  275  EDITOR=vi
+  276  crontab -e
+  277  crontab -r
+  278  crontab -l
+  279  clear
+  280  whoami
+  281  export TEST=1
+  282  echo $TEST
+  283  su oracle
+  284  sudo -l
+  285  whoami
+  286  pwd
+  287  clear
+  288  history
+  289  kill -9
+  290  clear
+  291  history
+[root@Sambe2025001 ~]$ !285
+whoami
+root
+[root@Sambe2025001 ~]$ echo $HISTSIZE
+500
+[root@Sambe2025001 ~]$ !!
+echo $HISTSIZE
+500
+[root@Sambe2025001 ~]$ !h
+history
+    1  pwd
+    2  PWD
+    3  cd
+    4  man cat
+    5  ls
+    6  exit
+    7  which cat
+    8  which tac
+    9  man -k calendar
+   10  path
+   11  ls
+   12  pwd
+   13  cd tpsreports
+   14  ls
+   15  echo #PATH
+   16  echo $PATH
+   17  which cat
+   18  cat sale.data
+   19  which cat
+   20  mk dir1
+   21  mkdir dir1
+   22  ls
+   23  explorer .exe .
+   24  explorer.exe .
+   25  Linux FundamentalsLinux FundamentalsLinux FundamentalsLinux Fundamentals
+   26  mkdir Linux-Fundamentals
+   27  cd Linux-Fundamentals/
+   28  touch file.txt
+   29  ls -la
+   30  echo "This is my Linux Fundamentals course practice" > file.txt
+   31  cat file.txt
+   32  clear
+   33  cat file.txt
+   34  clear
+   35  ls
+   36  ls -a
+   37  ls -l
+   38  ls -l -a
+   39  ls -a -l
+   40  clear
+   41  ls -l
+   42  ls -lr
+   43  ls -lat
+   44  ls -latr
+   45  ls -R
+   46  tree -d
+   47  ls -d
+   48  ls --color
+   49  ls -R
+   50  tree
+   51  clear
+   52  groups
+   53  Gn
+   54  id -Gn
+   55  clear
+   56  cd ..
+   57  mkdir testumask
+   58  cd testumask
+   59  umask -S
+   60  mkdir a-dir
+   61  touch a-file
+   62  ls -l
+   63  rmdir a-dir
+   64  rm a-file
+   65  umask 007
+   66  umask 0007
+   67  umask -S
+   68  mkdir a-dir
+   69  touch a-file
+   70  ls -l
+   71  cd ..
+   72  cd .
+   73  cd "C:\sc_ShriyaHariparsad_2026\Linux Fundamentals"
+   74  cd ..
+   75  cd Linux-Fundamentals
+   76  cd ~/Linux-Fundamentals/
+   77  clear
+   78  find
+   79  find .
+   80  find/usr/sbin -name networkmanager
+   81  find/usr/sbin -iname networkmanager
+   82  clear
+   83  locate uptime
+   84  find uptime
+   85  clear
+   86  cd ..
+   87  cd
+   88  cat
+   89  cd
+   90  cd mkdir my_folder
+   91  clear
+   92  mkdir my_test
+   93  cd my_test
+   94  touch myfile.txt
+   95  cat myfile.txt
+   96  echo "This is my first line" >myfile.txt
+   97  echo "This is the second" > myfile.txt
+   98  cat myfile.txt
+   99  echo "Here is some more interesting text" > myfile.txt
+  100  echo "Who is there" > myfile.txt
+  101  cat myfile.txt
+  102  clear
+  103  cat file.txt
+  104  clear
+  105  cd file.txt
+  106  cd my_test
+  107  clear
+  108  echo "This is a new line" >> myfile.txt
+  109  cat file.txt
+  110  mkdir my_file
+  111  cd my_file
+  112  touch file.txt
+  113  echo "This is the first line.
+  114  This is the second.
+  115  Here is some more interesting text.
+  116  Knock knock.
+  117  Who's there?
+  118  More filler text.
+  119  The quick brown fox jumps over the lazy dog.
+  120  The dog was rather impressed.
+  121  Roses are red,
+  122  Violets are blue,
+  123  All my base are belong to you.
+  124  Finally, the 12th and last line.
+  125  $ head file.txt
+  126  This is the first line.
+  127  This is the second.
+  128  Here is some more interesting text.
+  129  Knock knock.
+  130  Who's there?
+  131  More filler text.
+  132  The quick brown fox jumps over the lazy dog.
+  133  The dog was rather impressed.
+  134  Roses are red,
+  135  Violets are blue,
+  136  $ tail file.txt
+  137  Here is some more interesting text.
+  138  Knock knock.
+  139  Who's there?
+  140  More filler text.
+  141  The quick brown fox jumps over the lazy dog.
+  142  The dog was rather impressed.
+  143  Roses are red,
+  144  Violets are blue,
+  145  All my base are belong to you.
+  146  Finally, the 12th and last line.
+  147  $ more file.txt
+  148  Here is some more interesting text.
+  149  Knock knock.
+  150  Who's there?
+  151  More filler text." >> file.txt
+  152  cat myfile.txt
+  153  cat file.txt
+  154  head file.txt
+  155  head -2 file.txt
+  156  tail file.txt
+  157  tail -1 file.txt
+  158  nano file.txt
+  159  clear
+  160  vi file.txt
+  161  clear
+  162  emacs file.txt
+  163  emac file.txt
+  164  emacs file.txt
+  165  clear
+  166  echo $PSl
+  167  echo #PSl
+  168  PSl="<\t \u@\h \w>\$ "
+  169  echo $PS1
+  170  clear
+  171  echo $PS1
+  172  PS1="\u@\h \$ "
+  173  PS1="<\t \u@\h \w>\$ "
+  174  cd /var/log
+  175  PS1="\d \t \h \W>\$ "
+  176  cd
+  177  vi .bash_profile
+  178  exit
+  179   PS1="[\u@\h \W]\$"
+  180  clear
+  181   ls -l
+  182   alias ll='ls -l'
+  183  ll
+  184   ls -l
+  185  alias
+  186  alias
+  187  clear
+  188  printenv
+  189  printenv | less
+  190  printenv USER
+  191  echo $USER
+  192  date
+  193  export TZ="US/Pacific"
+  194  date
+  195  unset TZ
+  196  date
+  197  man date
+  198  clear
+  199  echo $PS1
+  200  cd ..
+  201   cd..
+  202  clear
+  203  echo $PS1
+  204  PS1="\u@\h \$ "
+  205  PS1="<\t \u@\h \w>\$"
+  206   cd /var/log
+  207   PS1="\d \t \h \W>\$ "
+  208  cd
+  209  vi .bash_profile
+  210  $echo $PS1
+  211  $PS1="\u@\h \$ "
+  212  PS1="[persist \u@\h \W]\$"
+  213  PS1="[persist \u@\h \W]\$ "
+  214  PS1="\t\n[\h \w]\$ "
+  215  clear
+  216  clear
+  217  sudo -i
+  218  sudo su -
+  219  clear
+  220  exit
+  221  PS1="[\u@\h \W]\$ "
+  222  clear
+  223  ps
+  224  ps -p 517
+  225  ps -f
+  226  ps -e
+  227  ps -ef
+  228  les
+  229  less
+  230  ps -ef |less
+  231  ps -fu root
+  232  ps --forest
+  233  ps -ef --forest
+  234  pstree
+  235  PS1"[\u@\h W]\$ "
+  236  $PS1="[\u@\h W]\$ "
+  237  PS1="[\u@\h W]\$ "
+  238  PS1="[\u@\h \W]\$ "
+  239  pstree
+  240  clear
+  241  pstree
+  242  top
+  243  htop
+  244  clear
+  245  ./long-running-program &
+  246  clear
+  247  jobs
+  248  ps -p
+  249  ./long-running-program &
+  250  ps -p4499
+  251  clear
+  252  ./long-running-program &
+  253  ps -p 4892
+  254  jobs
+  255  clear
+  256  kill -l
+  257  kill -KILL
+  258  kill -KILL ^C
+  259  clear
+  260  kill -l
+  261  kill -KILL ^C
+  262  clear
+  263  kill -l
+  264  ./long-running-program &
+  265  ./long-running-program &
+  266  kill 6489
+  267  kill -9
+  268  clear
+  269  crontab -l
+  270  vi my-cron
+  271  cat my-cron
+  272  crontab my-cron
+  273  crontab -l
+  274  echo $EDITOR
+  275  EDITOR=vi
+  276  crontab -e
+  277  crontab -r
+  278  crontab -l
+  279  clear
+  280  whoami
+  281  export TEST=1
+  282  echo $TEST
+  283  su oracle
+  284  sudo -l
+  285  whoami
+  286  pwd
+  287  clear
+  288  history
+  289  kill -9
+  290  clear
+  291  history
+  292  whoami
+  293  echo $HISTSIZE
+  294  echo $HISTSIZE
+  295  history
+[root@Sambe2025001 ~]$ !294
+echo $HISTSIZE
+500
+[root@Sambe2025001 ~]$ ls files.txt sorted_files.txt notes.txt
+ls: cannot access 'files.txt': No such file or directory
+ls: cannot access 'sorted_files.txt': No such file or directory
+ls: cannot access 'notes.txt': No such file or directory
+[root@Sambe2025001 ~]$ !e
+echo $HISTSIZE
+500
+[root@Sambe2025001 ~]$ echo !$
+echo $HISTSIZE
+500
+[root@Sambe2025001 ~]$ !l
+ls files.txt sorted_files.txt notes.txt
+ls: cannot access 'files.txt': No such file or directory
+ls: cannot access 'sorted_files.txt': No such file or directory
+ls: cannot access 'notes.txt': No such file or directory
+[root@Sambe2025001 ~]$ echo !$
+echo notes.txt
+notes.txt
+[root@Sambe2025001 ~]$ !l
+ls files.txt sorted_files.txt notes.txt
+ls: cannot access 'files.txt': No such file or directory
+ls: cannot access 'sorted_files.txt': No such file or directory
+ls: cannot access 'notes.txt': No such file or directory
+[root@Sambe2025001 ~]$ echo !$
+echo notes.txt
+notes.txt
+[root@Sambe2025001 ~]$ echo !^
+echo notes.txt
+notes.txt
+[root@Sambe2025001 ~]$ echo alpha bravo charlie
+alpha bravo charlie
+[root@Sambe2025001 ~]$ echo !^ !l:2 delta !l:0
+echo alpha sorted_files.txt delta ls
+alpha sorted_files.txt delta ls
+[root@Sambe2025001 ~]$ echo alpha sorted_files.txt delta ls
+alpha sorted_files.txt delta ls
+[root@Sambe2025001 ~]$ echo alpha sorted_files.txt delta ls
+alpha sorted_files.txt delta ls
+[root@Sambe2025001 ~]$ echo alpha sorted_files.txt delta ls
+alpha sorted_files.txt delta ls
+[root@Sambe2025001 ~]$ who
+who         whoami      whoami.exe
+[root@Sambe2025001 ~]$ who
+who         whoami      whoami.exe
+[root@Sambe2025001 ~]$ whoa
+Command 'whoa' not found, did you mean:
+  command 'who' from deb coreutils (9.4-2ubuntu2)
+Try: apt install <deb name>
+[root@Sambe2025001 ~]$ whoami
+root
+[root@Sambe2025001 ~]$ ls file
+ls: cannot access 'file': No such file or directory
+[root@Sambe2025001 ~]$ whoami
+root
+[root@Sambe2025001 ~]$

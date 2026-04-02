97729580fde27be265f576755a55035a756955f0
Shriya Hariparsad <shriya.hariparsad@sambeconsulting.com>
2026-04-02 09:46:08 +0200

    Section 4

---
+++ Python for Beginners/Section 4 39.py
@@ -0,0 +1,19 @@
+import time
+
+def connect_to_internet(signal: bool, delay: int) -> None:
+    if delay > 5:
+        signal = True
+
+    if signal:
+        print('Çonnected!')
+    else:
+        print(f'Connection failed. Trying again in: {delay}s... ')
+        time.sleep(delay)
+        connect_to_internet(signal, delay + 2)
+
+        connect_to_internet(False, 0)
+
+
+
+
+

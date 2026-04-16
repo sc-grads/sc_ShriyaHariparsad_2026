210831bb0a1b991c2e0cfa4d1eedc2ea5b353fcc
Shriya Hariparsad <shriya.hariparsad@sambeconsulting.com>
2026-04-16 15:23:46 +0200

    Python script for project

--- Project Sales Data Processing and Business Insights System/process_data.py
+++ Project Sales Data Processing and Business Insights System/process_data.py
@@ -8,8 +8,9 @@ logging.basicConfig(filename='../output/log.txt', level=logging.INFO, format='%(
 df = pd.read_csv('../data/Messy_Sales_Data.csv')
 logging.info("Python Messy_Sales_Data.csv loaded successfully")
 
-# Standardize column names
+# Standardise column names
 df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
+logging.info("Column names standardised, removed spaces and converted to lowercase, replaced spaces with underscores")
 
 # Define region and salesperson relationship
 region_map = {
@@ -18,9 +19,11 @@ region_map = {
     'East': 'David',
     'West': 'Sarah'
 }
+logging.info("Region and salesperson defined clearly")
 
 # Fill missing names based on region
 df['salesperson'] = df['salesperson'].fillna(df['region'].map(region_map))
+logging.info("Based on region, missing salesperson names filled successfully")
 
 # Calculate mean per category and round it up to nearest whole number
 df['quantity'] = df.groupby('category')['quantity'].transform(lambda x: x.fillna(int(x.mean() + 0.9999))).astype(int)
@@ -28,26 +31,30 @@ logging.info("Filled missing quantity values with rounded off averages")
 
 # Remove duplicates
 df = df.drop_duplicates()
+logging.info("Duplicate rows removed successfully")
 
-# Convert date column
+# Convert to datetime
 df['date'] = pd.to_datetime(df['date'], errors='coerce')
+logging.info("Converts the date column from a string to a datetime format")
 
-print(df.head())
+print(df.to_string())
 print(df.columns)
 print(df.info())
 print("Data cleaned successfully")
 
-# Create revenue column
+
+# Create Revenue column
 df['revenue'] = df['quantity'] * df['price']
 logging.info("Revenue column created successfully")
 
 # Month extracted from date
-df['month'] = df['date'].dt.to_period('M')
-logging.info("Month column extracted from date successfully")
-print(df.head())
+df['month'] = df['date'].dt.strftime('%B')
+logging.info("Month column extracted from date successfully using the full month name")
+print(df.to_string())
 
 # Save cleaned data
 df.to_csv('../output/clean_sales.csv', index=False)
+logging.info("Cleaned data saved successfully")
 
 # Aggregations
 sales_by_region = df.groupby('region')['revenue'].sum().reset_index()

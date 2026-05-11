059750a2163a5334d2e94d3263f6a146fa4d318e
Shriya Hariparsad <shriya.hariparsad@sambeconsulting.com>
2026-05-11 14:45:21 +0200

    Section 6 Query and Manipulation of Data using SQL - Difference between DELETE and Truncate statement

---
+++ Database_Administration/SQLQuery12.sql
@@ -0,0 +1,16 @@
+USE [AdventureWorks2025]
+GO
+
+INSERT INTO [dbo].[EmployeeNew]
+           ([EmployeeName])
+     VALUES
+           ('James')
+GO
+
+select * from EmployeeNew
+
+delete from EmployeeNew
+
+truncate table EmployeeNew
+
+

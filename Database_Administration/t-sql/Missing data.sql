4c296083a438cbef2cc9f5a7dfc4a915ff273199
Shriya Hariparsad <shriya.hariparsad@sambeconsulting.com>
2026-05-27 12:08:10 +0200

    Section 12: Session 2 - Find missing data, and delete and...

---
+++ Database_Administration/t-sql/Missing data.sql
@@ -0,0 +1,42 @@
+select E.EmployeeNumber as ENumber, E.EmployeeFirstName,
+       E.EmployeeLastName, T.EmployeeNumber as TNumber, 
+       sum(T.Amount) as TotalAmount
+from tblEmployee as E
+left join tblTransaction as T
+on E.EmployeeNumber = T.EmployeeNumber
+where T.EmployeeNumber IS NULL
+group by E.EmployeeNumber, T.EmployeeNumber, E.EmployeeFirstName,
+       E.EmployeeLastName
+order by E.EmployeeNumber, T.EmployeeNumber, E.EmployeeFirstName,
+       E.EmployeeLastName
+
+--RIGHT JOIN
+select ENumber, EmployeeFirstName, EmployeeLastName
+from (
+select E.EmployeeNumber as ENumber, E.EmployeeFirstName,
+       E.EmployeeLastName, T.EmployeeNumber as TNumber, 
+       sum(T.Amount) as TotalAmount
+from tblEmployee as E
+left join tblTransaction as T
+on E.EmployeeNumber = T.EmployeeNumber
+group by E.EmployeeNumber, T.EmployeeNumber, E.EmployeeFirstName,
+       E.EmployeeLastName) as newTable
+where TNumber is null
+order by ENumber, TNumber, EmployeeFirstName,
+       EmployeeLastName
+
+
+select *
+from (
+select E.EmployeeNumber as ENumber, E.EmployeeFirstName,
+       E.EmployeeLastName, T.EmployeeNumber as TNumber, 
+       sum(T.Amount) as TotalAmount
+from tblEmployee as E
+right join tblTransaction as T
+on E.EmployeeNumber = T.EmployeeNumber
+group by E.EmployeeNumber, T.EmployeeNumber, E.EmployeeFirstName,
+       E.EmployeeLastName) as newTable
+where ENumber is null
+order by ENumber, TNumber, EmployeeFirstName,
+       EmployeeLastName
+
